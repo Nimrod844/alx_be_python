c7d2088598f96040c1d5f2c0c44ddf46ec53e2d4
Nimrod844 <nimrodkogo1@gmail.comgit config --global user.email nimrodkogo1@gmail.com>
2025-07-08 13:59:22 +0300

    Add finace calculator script to compute monthly add projected annual saivngs

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
-income = float(input("enter your monthly income: "))
-expenses = float(input("enter your total monthly expenses: "))
-monthly_savings = income - expenses
-annual_savings = monthly_savings * 12
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your tatal monthly expenses: "))
+monthly_savings = monthly_income - monthly_expenses
+annual_savings = monthly_savings * 12 
 projected_savings = annual_savings + (annual_savings * 0.05)
-print(f"Your monthly savings are ${monthly_savings}")
-print(f"Projected saving after one year, with interest, is: ${projected_savings}")
+print(f"Your monthly savings are ${monthly_savings:.2f}.")
+print(f"Projected  savings after one year , with interest, is: ${projected_savings:.2f}.")
